80dbdcc8b45b0de49afd8fecb37c52a910bff677
fulkap740-blip <fulkap740@gmail.com>
2026-01-29 07:20:05 +0600

    Update handlers_admin.py

--- app/handlers_admin.py
+++ app/handlers_admin.py
@@ -1,8 +1,8 @@
 from aiogram import types
 from app.config import ADMIN_PASSWORD, OFFERS
-from app.excel import create_excel
+from app.db import DB_NAME, set_rate
+from app.excel import make_excel
 import sqlite3
-from app.db import DB_NAME
 
 admins = set()
 
@@ -12,7 +12,7 @@ async def gen_admin(message: types.Message):
 async def admin_auth(message: types.Message):
     if message.text == ADMIN_PASSWORD:
         admins.add(message.from_user.id)
-        await message.answer("Ты админ. /export")
+        await message.answer("Админ доступ получен")
     else:
         await message.answer("Неверный пароль")
 
@@ -23,5 +23,16 @@ async def export_excel(message: types.Message):
     with sqlite3.connect(DB_NAME) as conn:
         rows = conn.execute("SELECT * FROM requests").fetchall()
 
-    path = create_excel(rows)
+    path = make_excel(rows)
     await message.answer_document(types.FSInputFile(path))
+
+async def change_rate(message: types.Message):
+    if message.from_user.id not in admins:
+        return
+
+    try:
+        _, offer_id, rate = message.text.split()
+        set_rate(int(offer_id), float(rate))
+        await message.answer("Ставка обновлена")
+    except:
+        await message.answer("Формат: /rate <offer_id> <rate>")
