f5813a30ddad59a5589a67f227b30a50a61183f0
fulkap740-blip <fulkap740@gmail.com>
2026-01-29 07:55:34 +0600

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -4,10 +4,12 @@ from app.config import BOT_TOKEN
 from app.db import init_db
 from app.handlers_user import *
 from app.handlers_admin import *
+from app.states import RequestForm
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
+# USER
 dp.message.register(start, F.text == "/start")
 dp.callback_query.register(choose_offer, F.data.startswith("offer_"))
 dp.message.register(save_wallet, F.text.startswith("T"))
@@ -16,13 +18,17 @@ dp.message.register(step_video, RequestForm.video)
 dp.message.register(step_proof, RequestForm.proof)
 dp.message.register(step_views, RequestForm.views)
 
+# ADMIN (скрыто)
 dp.message.register(gen_admin, F.text == "/gen_admin")
 dp.message.register(admin_auth)
-dp.message.register(export_excel, F.text == "/export")
+dp.callback_query.register(admin_export, F.data == "admin_export")
+dp.callback_query.register(admin_exit, F.data == "admin_exit")
+
 
 async def main():
     init_db()
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
