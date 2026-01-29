1551c940526e78314043b90cdb8ae4d2f0d56fc9
fulkap740-blip <fulkap740@gmail.com>
2026-01-29 08:15:17 +0600

    Update handlers_admin.py

--- app/handlers_admin.py
+++ app/handlers_admin.py
@@ -1,24 +1,21 @@
 from aiogram import types
 from app.config import ADMIN_PASSWORD
 from app.keyboards import admin_menu
+from app.db import DB_NAME, set_rate
 from app.excel import create_excel
-from app.db import DB_NAME
 import sqlite3
 
 ADMINS = set()
 
 
 async def gen_admin(message: types.Message):
-    await message.answer("Введите пароль администратора:")
+    await message.answer("Введите пароль:")
 
 
 async def admin_auth(message: types.Message):
     if message.text == ADMIN_PASSWORD:
         ADMINS.add(message.from_user.id)
-        await message.answer("✅ Вы вошли в админку", reply_markup=admin_menu())
-    else:
-        # молча игнорируем
-        return
+        await message.answer("Админка", reply_markup=admin_menu())
 
 
 async def admin_export(call: types.CallbackQuery):
@@ -27,17 +24,9 @@ async def admin_export(call: types.CallbackQuery):
 
     with sqlite3.connect(DB_NAME) as conn:
         rows = conn.execute("""
-            SELECT
-                user_id,
-                offer,
-                video_link,
-                proof_link,
-                views,
-                amount,
-                status,
-                created
-            FROM requests
-            ORDER BY created DESC
+        SELECT user_id, offer, video_link, proof_link, views, amount, status, created
+        FROM requests
+        ORDER BY created DESC
         """).fetchall()
 
     path = create_excel(rows)
@@ -45,7 +34,14 @@ async def admin_export(call: types.CallbackQuery):
     await call.answer()
 
 
+async def admin_rate(call: types.CallbackQuery):
+    await call.message.answer(
+        "Формат:\nWhite Bird 1.7\nGenesis 2.3"
+    )
+    await call.answer()
+
+
 async def admin_exit(call: types.CallbackQuery):
     ADMINS.discard(call.from_user.id)
-    await call.message.answer("🚪 Вы вышли из админки")
+    await call.message.answer("Выход из админки")
     await call.answer()
