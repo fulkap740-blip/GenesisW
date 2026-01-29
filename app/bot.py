e3b2c4ce2945737d5b638840792bd72aefd76d86
fulkap740-blip <fulkap740@gmail.com>
2026-01-29 07:38:15 +0600

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,29 +1,18 @@
-import asyncio
-from aiogram import Bot, Dispatcher, F
-from app.config import BOT_TOKEN
-from app.db import init_db
 from app.handlers_user import *
 from app.handlers_admin import *
 
-bot = Bot(BOT_TOKEN)
-dp = Dispatcher()
-
 dp.message.register(start, F.text == "/start")
+dp.callback_query.register(profile, F.data == "profile")
+dp.callback_query.register(new_request, F.data == "new_request")
 dp.callback_query.register(choose_offer, F.data.startswith("offer_"))
 dp.message.register(save_wallet, F.text.startswith("T"))
-dp.callback_query.register(new_request, F.data == "new_request")
 dp.message.register(step_video, RequestForm.video)
 dp.message.register(step_proof, RequestForm.proof)
 dp.message.register(step_views, RequestForm.views)
+dp.callback_query.register(send_today, F.data == "send_today")
+dp.callback_query.register(help_cmd, F.data == "help")
+dp.callback_query.register(back, F.data == "back")
 
-dp.message.register(gen_admin, F.text == "/gen_admin")
-dp.message.register(admin_auth)
-dp.message.register(export_excel, F.text == "/export")
-dp.message.register(change_rate, F.text.startswith("/rate"))
-
-async def main():
-    init_db()
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+dp.message.register(gen_admin, F.text.startswith("/gen_admin"))
+dp.message.register(exit_admin, F.text == "/exit")
+dp.callback_query.register(admin_today, F.data == "admin_today")
