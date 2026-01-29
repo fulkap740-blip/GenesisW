a47a32cd8c23e6dd5502576599ab8aa63cc346d0
fulkap740-blip <fulkap740@gmail.com>
2026-01-29 06:59:16 +0600

    Update handlers_admin.py

--- app/handlers_admin.py
+++ app/handlers_admin.py
@@ -1,25 +1,27 @@
-from aiogram.types import Message
-from db import DB_NAME
-import aiosqlite
-from excel import build_excel
-from config import OFFERS
+from aiogram import types
+from app.config import ADMIN_PASSWORD, OFFERS
+from app.excel import create_excel
+import sqlite3
+from app.db import DB_NAME
 
-async def send_excel(bot, offer_id):
-    async with aiosqlite.connect(DB_NAME) as db:
-        rows = await db.execute_fetchall(
-            "SELECT user_id, offer_id, video, proof, views, amount, status FROM requests WHERE offer_id=?",
-            (offer_id,)
-        )
+admins = set()
 
-    requests = [
-        dict(zip(
-            ["user_id","offer_id","video","proof","views","amount","status"],
-            r
-        )) for r in rows
-    ]
+async def gen_admin(message: types.Message):
+    await message.answer("Введите пароль:")
 
-    file = build_excel(requests)
-    await bot.send_document(
-        OFFERS[offer_id]["admin_id"],
-        document=open(file, "rb")
-    )
+async def admin_auth(message: types.Message):
+    if message.text == ADMIN_PASSWORD:
+        admins.add(message.from_user.id)
+        await message.answer("Ты админ. /export")
+    else:
+        await message.answer("Неверный пароль")
+
+async def export_excel(message: types.Message):
+    if message.from_user.id not in admins:
+        return
+
+    with sqlite3.connect(DB_NAME) as conn:
+        rows = conn.execute("SELECT * FROM requests").fetchall()
+
+    path = create_excel(rows)
+    await message.answer_document(types.FSInputFile(path))
