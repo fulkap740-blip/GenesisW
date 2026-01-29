c2eac999ef6856ef6bdd648f8eb3a97ce46cf2cb
fulkap740-blip <fulkap740@gmail.com>
2026-01-29 07:19:17 +0600

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,14 +1,17 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from app.config import OFFERS
 
-def offer_keyboard():
-    buttons = [
-        [InlineKeyboardButton(text=v["name"], callback_data=f"offer_{k}")]
-        for k, v in OFFERS.items()
-    ]
-    return InlineKeyboardMarkup(inline_keyboard=buttons)
+def offers_kb():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text=v["name"], callback_data=f"offer_{k}")]
+            for k, v in OFFERS.items()
+        ]
+    )
 
-def main_menu():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="➕ Новая заявка", callback_data="new_request")]
-    ])
+def user_menu():
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="➕ Новая заявка", callback_data="new_request")]
+        ]
+    )
