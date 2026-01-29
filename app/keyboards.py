d2e8b007a9b21d682eb77bb48e3b8812ee738436
fulkap740-blip <fulkap740@gmail.com>
2026-01-29 07:52:38 +0600

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,25 +1,25 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from app.config import OFFERS
+
+
+def offer_keyboard():
+    keyboard = []
+    for k, v in OFFERS.items():
+        keyboard.append(
+            [InlineKeyboardButton(
+                text=v["name"],
+                callback_data=f"offer_{k}"
+            )]
+        )
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
 
 def main_menu():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="👤 Профиль", callback_data="profile")],
-        [InlineKeyboardButton(text="➕ Новая заявка", callback_data="new_request")],
-        [InlineKeyboardButton(text="🧾 Это все мои заявки на сегодня", callback_data="send_today")],
-        [InlineKeyboardButton(text="ℹ️ Help", callback_data="help")]
-    ])
-
-def back_menu():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="⬅️ Назад", callback_data="back")]
-    ])
-
-def offers_kb():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="White Bird", callback_data="offer_1")],
-        [InlineKeyboardButton(text="Genesis", callback_data="offer_2")]
-    ])
-
-def admin_menu():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="📂 Заявки за сегодня", callback_data="admin_today")]
-    ])
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text="➕ Новая заявка",
+                callback_data="new_request"
+            )]
+        ]
+    )
