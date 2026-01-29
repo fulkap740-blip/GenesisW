5863a69f8b5239185a9bea6f309f75f3390aa8c1
fulkap740-blip <fulkap740@gmail.com>
2026-01-29 06:43:20 +0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 
 OFFERS = {
-    1: {"name": "White Bird", "rate": 1.5},
-    2: {"name": "Genesis", "rate": 2.0}
+    1: {"name": "White Bird", "rate": 1.5, "admin_id": 111111111},
+    2: {"name": "Genesis", "rate": 2.0, "admin_id": 222222222},
 }
