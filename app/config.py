0387e1330919aa8fcaae26cf17b11c5e5f48d3b9
fulkap740-blip <fulkap740@gmail.com>
2026-01-30 12:28:43 +0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,6 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 
 OFFERS = {
-    1: {"name": "White Bird", "rate": 1.5, "admin_id": 111111111},
-    2: {"name": "Genesis", "rate": 2.0, "admin_id": 222222222},
+    "White Bird": {"rate": 1.5},
+    "Genesis": {"rate": 2.0},
 }
